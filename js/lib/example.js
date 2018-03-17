6237a3c97bc98e8e1e404b544ca706375bab6d5e
cornhundred <nickfloresfernandez@gmail.com>
2018-03-16 21:28:16 -0400

    added license

--- js/lib/example.js
+++ js/lib/example.js
@@ -33,7 +33,8 @@ var HelloModel = widgets.DOMWidgetModel.extend({
 // Custom View. Renders the widget model.
 var HelloView = widgets.DOMWidgetView.extend({
     render: function() {
-        console.log('actually working')
+        console.log('example.js *************');
+        console.log('--------------');
         this.value_changed();
         this.model.on('change:value', this.value_changed, this);
     },
